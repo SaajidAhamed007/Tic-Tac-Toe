84370fbdcb62326f53268bb40bda5af3fa710c62
SaajidAhamed007 <saajidahamed01@gmail.com>
2025-07-13 17:46:11 +0530

    ui fixing

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ boxes.forEach((box)=>{
         game[box.getAttribute("data-key")]=player
         if(box.getAttribute("data-value")){
             alert("already filled")
+            count--;
+            return;
         }
         else if(player==="x"){
             const xbox =  box.querySelector(".mark-x");
@@ -26,11 +28,9 @@ boxes.forEach((box)=>{
             player="x";
         }
 
-        console.log(game);
-        console.log(game[0]==game[1] && game[1]==game[2])
+        console.log(game)
 
-        const matchwin = (game[0]==game[1] && game[1]==game[2] && game[0]!="") || (game[3]==game[4] && game[4]==game[5] && game[3]!="") || (game[6]==game[7] && game[7]==game[8] && game[6]!="") || (game[0]==game[4] && game[4]==game[8] && game[0]!="") || (game[2]==game[4] && game[4]==game[6] && game[2]!="") || (game[0]==game[3] && game[6]==game[3] && game[0]!="") || (game[1]==game[4] && game[1]==game[7] && game[1]!="") || (game[2]==game[5] && game[5]==game[8] && game[2]!="")
-        console.log(matchwin)
+        const matchwin = (game[0]===game[1] && game[1]===game[2] && game[0]!="") || (game[3]===game[4] && game[4]===game[5] && game[3]!="") || (game[6]===game[7] && game[7]===game[8] && game[6]!="") || (game[0]===game[4] && game[4]===game[8] && game[0]!="") || (game[2]===game[4] && game[4]===game[6] && game[2]!="") || (game[0]===game[3] && game[3]===game[6] && game[0]!="") || (game[1]===game[4] && game[4]===game[7] && game[1]!="") || (game[2]===game[5] && game[5]===game[8] && game[2]!="")
         
         if(matchwin == true){
             setTimeout(()=>{
@@ -39,10 +39,11 @@ boxes.forEach((box)=>{
                 window.location.reload();
             },(100))
         }
-
-        if(count === 9 ){
-            alert("Match Draw")
-            window.location.reload();
+        else if(count === 9 ){
+            setTimeout(()=>{
+                alert("Match Draw")
+                window.location.reload();
+            },(200))
         }
 
     })
